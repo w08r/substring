c3cc402d275d49cb9d1a78ad8d6f2efcdbecde3c
Will O'Brien <will.08rien@gmail.com>
2022-03-23 12:47:20 +0000

    Initial commit

---
+++ src/substring.clj
@@ -0,0 +1,31 @@
+(ns substring
+  (:require [clojure.string :as str]
+            [clojure.spec.alpha :as spec]))
+
+(defn- substring [text start len]
+  (let [c (.toCharArray text)]
+    (str/join (map #(nth c %) (range start (+ start len))))))
+
+(defn- slider
+  ([text window-len] (slider text window-len 0))
+  ([text window-len cur] (lazy-seq
+                          (when (<= (+ cur window-len) (.length text))
+                            (cons (substring text cur window-len)
+                                  (slider text window-len (inc cur)))))))
+
+(spec/def :substring/find-args (spec/cat :needle string? :haystack string?))
+
+(spec/fdef strpos
+  :args :substring/find-args
+  :fn #(and (<= (:ret %) (.length (-> % :args :haystack)))
+            (or (>= (:ret %) 0) (= -1 (:ret %))))
+  :ret int?)
+
+(defn strpos [needle haystack]
+  "Search for needle as a substring of haystack.
+   Return the first position found or -1 if not present."
+  (-> (loop [p 0 [fst & rst] (slider haystack (.length needle))]
+        (condp
+          (= fst needle) p
+          rst (recur (inc p) rst)
+          :else -1))))
---
+++ test/substring_test.clj
@@ -0,0 +1,44 @@
+(ns substring-test
+   (:require [substring :as ss]
+             [clojure.spec.alpha :as s]
+             [clojure.test.check.generators :as gen]
+             [clojure.spec.test.alpha :as stest]))
+
+(use 'clojure.test)
+
+(defn instr-fns [f]
+  (stest/instrument `ss/strpos)
+  (f)
+  (stest/unstrument `ss/strpos))
+
+(use-fixtures :once instr-fns)
+
+(def str-gen #(gen/tuple
+          (gen/fmap clojure.string/join
+                    (gen/vector
+                     (gen/elements ["a" "b"])))
+          (gen/fmap clojure.string/join
+                    (gen/vector
+                     (gen/elements ["a" "b" "c"])))))
+
+
+(defn pass? [t]
+  (every? #(not (:failure %)) t))
+
+(deftest check-strpos
+  (is (and
+       (pass? (stest/check `ss/strpos {:gen {:substring/find-args str-gen}}))
+       (pass? (stest/check `ss/strpos)))))
+
+(deftest basic
+  (is (= (ss/strpos "abcd" "abcabcdabcde") 3))
+  (is (= (ss/strpos "abc" "abcabcdabcde") 0))
+  (is (= (ss/strpos "z" "abcabcdabcde") -1)))
+
+(deftest empty-strings
+  (is (= (ss/strpos "" "") 0))
+  (is (= (ss/strpos "a" "") -1))
+  (is (= (ss/strpos "" "a") 0)))
+
+(deftest null-inputs
+  (is (thrown? clojure.lang.ExceptionInfo (ss/strpos nil nil) -1)))
