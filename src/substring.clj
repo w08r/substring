ffb1531bc0a4cd966b3a6216097e870a5312ee64
Will O'Brien <will.08rien@gmail.com>
2022-03-25 16:23:13 +0000

    Fixed hints

--- src/substring.clj
+++ src/substring.clj
@@ -2,13 +2,13 @@
   (:require [clojure.string :as str]
             [clojure.spec.alpha :as spec]))
 
-(defn- substring [text start len]
+(defn- substring [^String text start len]
   (let [c (.toCharArray text)]
     (str/join (map #(nth c %) (range start (+ start len))))))
 
 (defn- slider
-  ([text window-len] (slider text window-len 0))
-  ([text window-len cur] (lazy-seq
+  ([^String text window-len] (slider text window-len 0))
+  ([^String text window-len cur] (lazy-seq
                           (when (<= (+ cur window-len) (.length text))
                             (cons (substring text cur window-len)
                                   (slider text window-len (inc cur)))))))
@@ -22,7 +22,7 @@
               (or (>= r 0) (= r -1))))
   :ret int?)
 
-(defn ^Long strpos [^CharSequence needle ^CharSequence haystack]
+(defn ^Long strpos [^String needle ^String haystack]
   "Search for needle as a substring of haystack.
    Return the first position found or -1 if not present."
   (-> (loop [p 0 [fst & rst] (slider haystack (.length needle))]
@@ -30,3 +30,9 @@
           (= fst needle) p
           rst (recur (inc p) rst)
           :else -1))))
+
+(comment
+  (strpos "a" "baa")                    ;=> 1
+  (strpos "c" "baa")                    ;=> -1
+  (strpos "abcd" "abcabcdabcde")        ;=> 3
+)
